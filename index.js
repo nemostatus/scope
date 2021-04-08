d5b28927fce19ce5e96b84c47cbb658d2ca42e7f
nemostatus <mattcorralejo1@gmail.com>
2021-04-07 17:42:53 -0700

    adding an example of scope

---
+++ index.js
@@ -0,0 +1,15 @@
+let revealed = document.getElementById("reveal")
+function revealX(){
+    let x = 5
+    if(x>3){
+ console.log("x is more than 3")
+    }
+    else{
+        console.log("x is less than 3")
+    }
+}
+let x = 1
+
+revealX()
+//in the console "x is more than 3" is revealed. 
+//in the creation phase 
