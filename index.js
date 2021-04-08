746284a5589cfa7d515ff4635338d9dc9f33741e
nemostatus <mattcorralejo1@gmail.com>
2021-04-07 17:50:31 -0700

    adding notes to help understand js engine a bit more

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ let x = 1
 
 revealX()
 //in the console "x is more than 3" is revealed. 
-//in the creation phase 
+//in the creation phase all of the variables have a space in memory as well as the function
+//what matters is the context of everything surrounding the variable. Execution context.
